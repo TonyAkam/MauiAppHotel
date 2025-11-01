c47ae0d8d57cade493475590944df418f823ca49
tony <ton@etec.com>
2025-11-01 17:39:47 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+#if !AUTO_GENERATED_INITIALIZECOMPONENT
+            // Remova ou comente esta linha se o método InitializeComponent gerado automaticamente já existe.
+            // Caso contrário, mantenha apenas uma definição de InitializeComponent.
+           InitializeComponent();
+#endif
+
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
+        }
+
+        // Remova este método se houver um método InitializeComponent gerado automaticamente pelo XAML.
+        // Caso contrário, mantenha apenas uma definição deste método.
+        // private void InitializeComponent()
+        // {
+        //     throw new NotImplementedException();
+        // }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        }
+    }
+}
